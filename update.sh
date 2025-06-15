236cd47424950f0cbd5c075c8991c3d8b1a05f98
xJayMorex <github@johntaylor.hu>
2025-06-15 22:17:19 +0200

    Update script cleanup

--- update.sh
+++ update.sh
@@ -7,16 +7,16 @@ fi
 
 echo "Updating ProtonPlus Void from version ${1} to version ${2}..."
 
-git checkout -q origin/master -b "v${2/+/-}"|| { echo "Unable to create branch ${2}"; exit 2; }
+git checkout -q origin/master -b "v${2/+/-}" || { echo "Unable to create branch ${2}"; exit 2; }
 
 sed -i "s/version=${1/-/+}/version=${2/-/+}/" void-packages/srcpkgs/protonplus/template
 
 wget -q https://github.com/Vysp3r/ProtonPlus/archive/refs/tags/v${1/+/-}.tar.gz -P /tmp/ > /dev/null || { echo "Unable to download ProtonPlus v${1/+/-}.tar.gz"; exit 3; }
-OLD_PROTONPLUS_HASH=`sha256sum /tmp/v${1/+/-}.tar.gz | awk '{print $1}'`
+OLD_PROTONPLUS_HASH=`sha256sum /tmp/v${1/+/-}.tar.gz | awk '{ print $1 }'`
 rm /tmp/v${1/+/-}.tar.gz
 
 wget -q https://github.com/Vysp3r/ProtonPlus/archive/refs/tags/v${2/+/-}.tar.gz -P /tmp/ > /dev/null || { echo "Unable to download ProtonPlus v${2/+/-}.tar.gz"; exit 4; }
-PROTONPLUS_HASH=`sha256sum /tmp/v${2/+/-}.tar.gz | awk '{print $1}'`
+PROTONPLUS_HASH=`sha256sum /tmp/v${2/+/-}.tar.gz | awk '{ print $1 }'`
 rm /tmp/v${2/+/-}.tar.gz
 
 sed -i "s/$OLD_PROTONPLUS_HASH/$PROTONPLUS_HASH/" void-packages/srcpkgs/protonplus/template
